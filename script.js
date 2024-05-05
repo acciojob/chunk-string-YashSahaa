a4148f3db59079d8a97a6528cbc229cf97569317
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-05 22:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	let arr = []
+	let i = 0
+	let count = 0
+	for (let s of str) {
+		if(count==size){
+			i++
+			count=0
+		}
+		if(arr[i]!=undefined){
+			arr[i]+=s
+		}
+		else arr.push(s)
+		count++
+	}
+	return arr
 }
 
 // Do not change the code below
